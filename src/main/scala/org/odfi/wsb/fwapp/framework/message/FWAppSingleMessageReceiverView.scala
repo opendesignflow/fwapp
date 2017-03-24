38fe149315f62a852fc91155d460fbc2d9a81c24
richnou <leys.richard@gmail.com>
2017-03-24 20:37:27 +0100

    Remvoed debugging

--- src/main/scala/org/odfi/wsb/fwapp/framework/message/FWAppSingleMessageReceiverView.scala
+++ src/main/scala/org/odfi/wsb/fwapp/framework/message/FWAppSingleMessageReceiverView.scala
@@ -9,7 +9,7 @@ trait FWAppSingleMessageReceiverView extends FWappView {
 
   def singleMessageReceive[MT <: SingleMessage](onlyLast: Boolean = false, delayMS: Long = 100)(cl: (MT => Unit))(implicit tag: ClassTag[MT]) = {
 
-    println("********** Preparing Single Message Receiver")
+    //println("********** Preparing Single Message Receiver")
     
     val int = new SingleMessageIntermediary[MT] {
 
