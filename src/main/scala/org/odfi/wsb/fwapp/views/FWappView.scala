1f411388233e17d4d75830471f77d05558b218a8
richnou <leys.richard@gmail.com>
2017-04-20 10:38:21 +0200

    Updatef or sub view support

--- src/main/scala/org/odfi/wsb/fwapp/views/FWappView.scala
+++ src/main/scala/org/odfi/wsb/fwapp/views/FWappView.scala
@@ -11,134 +11,143 @@ import java.net.URLEncoder
 import org.odfi.wsb.fwapp.Site
 import scala.collection.convert.DecorateAsJava
 import scala.collection.convert.DecorateAsScala
-
+import scala.reflect.ClassTag
 
 trait FWappView extends BasicHTMLView with HarvestedResource with DecorateAsJava with DecorateAsScala {
-  
+
   // ID and so on
   //---------------
   def getId = getClass.getCanonicalName
-  
-  def getUniqueId = getId+""+hashCode()
-  
-  
-  
+
+  def getUniqueId = getId + "" + hashCode()
+
   // Environment utilities -> finding main Site and so on
   //---------------
 
-  
-  def getFirstIntermediary =  this.findUpchainResource[FWappIntermediary]
-  
+  def getFirstIntermediary = this.findUpchainResource[FWappIntermediary]
+
   def getApp = this.findUpchainResource[FWappIntermediary] match {
-    case Some(baseIntermediary) => 
-      baseIntermediary.findParentOfType[Site] 
-    case None => 
+    case Some(baseIntermediary) =>
+      baseIntermediary.findParentOfType[Site]
+    case None =>
       None
   }
-  
-  def getIntermediaryTreeTop =  this.findUpchainResource[FWappIntermediary] match {
-    case Some(baseIntermediary) => 
+
+  def getIntermediaryTreeTop = this.findUpchainResource[FWappIntermediary] match {
+    case Some(baseIntermediary) =>
       Some(baseIntermediary.findTopMostIntermediary)
-    case None => 
+    case None =>
       None
   }
   
+  // Rendering
+  //--------------
+  override def rerender = {
+    this.cleanDerivedResourcesOfType[FWappView]
+    super.rerender
+  }
+
   // Requests
   //--------
-  var request  : Option[HTTPRequest] = None
-  
+  var request: Option[HTTPRequest] = None
+
   // URI/Path Utils
   //-----------
-  
-  def createFullDomainURI(pathInput:String) = {
-    
+
+  def createFullDomainURI(pathInput: String) = {
+
     s"http://${request.get.getURLParameter("Host").get}${createAppURI(pathInput)}"
-    
+
   }
-  def createAppURI(pathInput:String) = { 
-    
-    val path = pathInput.replaceAll("//+","/")
-    
-    
-    //println("createAppURI for -> "+path)
-    var resLink = getApp match {
-    
-      //-- Start with # , don't change
-      case Some(site : Site) if(path.startsWith("#")) => 
-        new URI(path.replaceAll("//+","/"))
-      //-- Starts with @/, path based on top level app
-      case Some(site : Site) if(path.startsWith("@/")) => 
-        
-        //println("Found @/ link;  app is: "+site)
-        
-        // Take app, and look for top most from it
-        site.findTopMostIntermediaryOfType[Site] match {
-          case Some(topSite) =>
-            
-             //println("Found @/ link; top app is: "+topSite+" -> "+topSite.fullURLPath)
-            
-             new URI( (topSite.fullURLPath+"/"+ path.stripPrefix("@").stripPrefix(topSite.fullURLPath) ).replaceAll("//+","/"))
-          case otherwise => 
-            // println("Found @/ link without top app, adding current app base: "+site.fullURLPath)
-            
-            new URI( (site.fullURLPath+"/"+path.stripPrefix("@").stripPrefix(site.fullURLPath)).replaceAll("//+","/"))
+  def createAppURI(pathInput: String) = {
+
+    pathInput match {
+      //-- Start with http: -> external
+      case external if (external.startsWith("http://")) =>
+        new URI(pathInput)
+      case other =>
+        val path = pathInput.replaceAll("//+", "/")
+        //println("createAppURI for -> "+path)
+        var resLink = getApp match {
+
+          //-- Start with # , don't change
+          case Some(site: Site) if (path.startsWith("#")) =>
+
+            new URI(path.replaceAll("//+", "/"))
+
+          //-- Starts with @/, path based on top level app
+          case Some(site: Site) if (path.startsWith("@/")) =>
+
+            //println("Found @/ link;  app is: "+site)
+
+            // Take app, and look for top most from it
+            site.findTopMostIntermediaryOfType[Site] match {
+              case Some(topSite) =>
+
+                //println("Found @/ link; top app is: "+topSite+" -> "+topSite.fullURLPath)
+
+                new URI((topSite.fullURLPath + "/" + path.stripPrefix("@").stripPrefix(topSite.fullURLPath)).replaceAll("//+", "/"))
+              case otherwise =>
+                // println("Found @/ link without top app, adding current app base: "+site.fullURLPath)
+
+                new URI((site.fullURLPath + "/" + path.stripPrefix("@").stripPrefix(site.fullURLPath)).replaceAll("//+", "/"))
+            }
+
+          case Some(site: Site) if (!path.startsWith("/")) =>
+            //println("Path is relative")
+            new URI(getViewPath + path)
+          case Some(site: Site) if (!path.startsWith(site.fullURLPath)) =>
+            // println("Path does not start with site base path: "+site.fullURLPath)
+            new URI((site.fullURLPath + "/" + path).replaceAll("//+", "/"))
+          case other => new URI(path.replaceAll("//+", "/"))
         }
-        
-       
-        
-      case Some(site : Site) if(!path.startsWith("/")) => 
-        //println("Path is relative")
-        new URI(getViewPath+path)
-      case Some(site : Site) if(!path.startsWith(site.fullURLPath)) =>   
-       // println("Path does not start with site base path: "+site.fullURLPath)
-        new URI((site.fullURLPath+"/"+path).replaceAll("//+","/"))
-      case other => new URI(path.replaceAll("//+","/"))
+        //println("Result link: "+resLink)
+        resLink
+
     }
-    //println("Result link: "+resLink)
-    resLink
+
   }
-  def createAssetsResolverURI(path:String) = {
+  def createAssetsResolverURI(path: String) = {
     new URI(getApp match {
-      case Some(app) if (app.assetsResolver.isDefined) => 
-        
+      case Some(app) if (app.assetsResolver.isDefined) =>
+
         //-- Found assets resolver and use full URL
-        
-        
-       // println(s"Assets are in : "+app.assetsResolver.get.fullURLPath)
-        
-        ("/"+app.assetsResolver.get.fullURLPath+"/"+path).replaceAll("//+","/")
+
+        // println(s"Assets are in : "+app.assetsResolver.get.fullURLPath)
+
+        ("/" + app.assetsResolver.get.fullURLPath + "/" + path).replaceAll("//+", "/")
       case other =>
         println(s"No app/assets resolver in upchain")
-        path.replaceAll("//+","/")
+        path.replaceAll("//+", "/")
     })
   }
-  
+
   def getAssetsResolver = getApp match {
-      case Some(app) => 
-        app.assetsResolver
-      case other => 
-        None
+    case Some(app) =>
+      app.assetsResolver
+    case other =>
+      None
   }
-  
+
   /**
    * Create a link to current view, with associated URL Arguments
    */
-  def createCurrentViewLink(args:(String,String)*) = {
-    
+  def createCurrentViewLink(args: (String, String)*) = {
+
     args.size match {
-      case 0  => 
+      case 0 =>
         getViewPath
-      case other => 
-        getViewPath+"?"+args.map { case (name,value) => s"$name=${URLEncoder.encode(value,"UTF8")}"}.mkString("&")
+      case other =>
+        getViewPath + "?" + args.map { case (name, value) => s"$name=${URLEncoder.encode(value, "UTF8")}" }.mkString("&")
     }
-    
-    
+
   }
-  
+
   def getViewPath = {
+    //println("Looking for First FwappIntermediary on "+getClass.getName+" -> "+parentResource)
     this.findUpchainResource[FWappIntermediary].get.fullURLPath
   }
-  
+
   /**
    * Returns sub path of current view
    * If view is at /a/b , and the request is responded at /a/b/c/d/e
@@ -148,27 +157,72 @@ trait FWappView extends BasicHTMLView with HarvestedResource with DecorateAsJava
     request.get.originalPath.stripPrefix(getViewPath)
   }
   
+  // Sub view utils
+  //-----------------
+  
+  /**
+   * Returns the Top View in the resource chain
+   * Useful if the current view is placed within another view
+   */
+  def getTopView[T <: FWappView : ClassTag] = this.findTopMostResource[T] 
+  
+  def getTopViewOrSelf = this.findTopMostResource[FWappView] match {
+    case Some(top) => top
+    case None => this
+    
+  }
+  
+  def getTopViewOrSelfAs[T <: FWappView : ClassTag]  = this.findTopMostResource[T] match {
+    case Some(top) => top
+    case None => this.asInstanceOf[T]
+    
+  }
+  
+  override def addView[T <: BasicHTMLView](v:T) = {
+     
+    //-- View must be derived from this one
+    //println("Add view of: "+v)
+    v match {
+      case fv : FWappView => 
+        //println("Add F view of: "+fv)
+         fv.deriveFrom(this)
+      case other => 
+        
+    }
+   
+    
+    //-- Parent call
+    val res = super.addView(v)
+    //println(s"Node add result to ${currentNode.hashCode()}: parent is ${res.parent.hashCode()} :"+res.parent.isDefined)
+    res
+    
+  }
+
+
   // Session Utils
   //----------------
   def ensureSession = request match {
-    case Some(req) => 
+    case Some(req) =>
       req.getSession
-    case None => 
+    case None =>
   }
-  
+
   // HTML Overrides
   //---------------------
-  
+
   /**
    * Make Links automatically adapted to application
    */
-  override def a(target:String)(cl: => Any) = {
-    
-   // println(s"OVERRIDEN A LINK")
+  override def a(target: String)(cl: => Any) = {
+
+    // println(s"OVERRIDEN A LINK")
     super.a(createAppURI(target).toString())(cl)
-    
+
   }
-  
-  
-  
+
+  def aToBlank(target: String, s: String) = a(target) {
+    text(s)
+    +@("target" -> "_blank")
+  }
+
 }
