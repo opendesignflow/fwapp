d81725729103a923b12768941efc97c3501f32bf
richnou <leys.richard@gmail.com>
2017-04-20 17:45:31 +0200

    Added findSiteBase folder utility to find the project location based on finding the main site class location...useful if the tool compiles into "target/classes", experimental for now

--- src/main/scala/org/odfi/wsb/fwapp/SiteApp.scala
+++ src/main/scala/org/odfi/wsb/fwapp/SiteApp.scala
@@ -9,7 +9,6 @@ import org.odfi.wsb.fwapp.swing.SwingPanelSite
 
 trait DelayedIndesignStart extends DelayedInit {
 
- 
 }
 class SiteApp(path: String) extends SwingPanelSite(path) with App {
 
@@ -21,7 +20,7 @@ class SiteApp(path: String) extends SwingPanelSite(path) with App {
     IndesignPlatorm use this
     IndesignPlatorm.start
   }*/
-  
+
   def start = {
     IndesignPlatorm use this
     IndesignPlatorm.start
@@ -29,17 +28,16 @@ class SiteApp(path: String) extends SwingPanelSite(path) with App {
 
 }
 
-class DefaultSiteApp(path: String) extends SiteApp(path)  {
+class DefaultSiteApp(path: String) extends SiteApp(path) {
 
   //-- Setup default assets
   val assetsManager = this.useDefaultAssets
 
-
   //-- Setup default config
   IndesignPlatorm use OOXOOConfigModule
   OOXOOConfigModule.configFolder = new File(s"config/$path")
-  
- /* override def delayedInit(body: => Unit) = {
+
+  /* override def delayedInit(body: => Unit) = {
     super.delayedInit(body)
     println("Now SiteApp delayed start")
     body
@@ -47,9 +45,45 @@ class DefaultSiteApp(path: String) extends SiteApp(path)  {
     IndesignPlatorm use this
     IndesignPlatorm.start
   }*/
-  
+
   this.onStart {
     assetsManager.addDefaultResourcesAssetSource("/nadc")
+  }
+
+  // Utilities
+  //----------
+
+  /**
+   * Lookup
+   */
+  def findSiteBaseFolder = {
+    var cl = getClass.getClassLoader
+    var searchPath = getClass.getCanonicalName.replace(".", "/") + ".class"
+    
+    //println("Searching for: " + searchPath)
+
+    var classResource = cl.getResource(searchPath)
+   // println("Found: " + classResource.getFile)
+
+    classResource match {
+
+      case path if (path != null && new File(path.getFile).exists()) =>
+        
+        //-- Remove search part from file, and we'll have the folder
+        Some(new File(path.getFile.stripSuffix(searchPath).stripSuffix("target/classes/")).getCanonicalFile)
+        
+      case other => None
+    }
+ 
+  }
+  
+  def findInSiteBaseFolder(path:String) = {
+    findSiteBaseFolder match {
+      case None => None
+      case Some(f) => 
+        //println("Site is :" +f)
+        Some(new File(f,path).getCanonicalFile)
+    }
   }
 
 }
